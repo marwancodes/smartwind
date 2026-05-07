812068f978a6f01f5933c128b38162f397a0173b
marwancoder <marwane.ouarradi@gmail.com>
2026-05-07 17:59:26 +0100

    Add environment variables and integrate Clerk authentication in frontend and backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,35 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express';
+import { clerkWebhookHandler } from './webhooks/clerk';
+import { getEnv } from './lib/env';
 
 dotenv.config();
 
+const env = getEnv();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const rawJson = express.raw({ type: 'application/json', limit: '1mb' });
+
+app.post('/webhook/clerk', rawJson, (req, res) => {
+  // it's important that you don't parse the webhook event data, it should be in raw format
+  void clerkWebhookHandler(req, res);
+});
+
+
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: http://localhost:${PORT}`);
+
+
+app.listen(env.PORT, () => {
+  console.log(`Server is running on port: http://localhost:${env.PORT}`);
 });
---
+++ backend/src/webhooks/clerk.ts
@@ -0,0 +1,6 @@
+import type { Request, Response } from "express";
+
+
+export async function clerkWebhookHandler(req: Request, res: Response) {
+
+}
